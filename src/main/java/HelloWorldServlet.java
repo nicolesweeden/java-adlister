d4469c81a1e421481c0cf560a3e2bfcda1ab6560
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-02-02 23:25:28 -0700

    testing if commit went through

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//why is my gh not showing commit
+
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
